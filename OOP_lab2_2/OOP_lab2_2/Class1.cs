4062154722b56666a72e9f8f1c277de5c3268584
Ihor-Bilan <120909956+Ihor-Bilan@users.noreply.github.com>
2024-12-19 17:31:57 +0200

    1

---
+++ OOP_lab2_2/OOP_lab2_2/Class1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+public class MyTime
+{
+    private int hour;
+    private int minute;
+    private int second;
+
+    public MyTime(int h, int m, int s)
+    {
+        if (h < 0 || h > 23)
+            throw new ArgumentOutOfRangeException("hour", "Hour must be between 0 and 23");
+        if (m < 0 || m > 59)
+            throw new ArgumentOutOfRangeException("minute", "Minute must be between 0 and 59");
+        if (s < 0 || s > 59)
+            throw new ArgumentOutOfRangeException("second", "Second must be between 0 and 59");
+
+        hour = h;
+        minute = m;
+        second = s;
+    }
+
+    public override string ToString()
+    {
+        return $"{hour}:{minute:D2}:{second:D2}";
+    }
+
+    public int TimeSinceMidnight()
+    {
+        return hour * 3600 + minute * 60 + second;
+    }
+
+    public static MyTime TimeSinceMidnight(int t)
+    {
+        int secPerDay = 86400;
+        t %= secPerDay;
+        if (t < 0)
+            t += secPerDay;
+        int h = t / 3600;
+        int m = (t / 60) % 60;
+        int s = t % 60;
+        return new MyTime(h, m, s);
+    }
+
+    public void AddOneSecond()
+    {
+        AddSeconds(1);
+    }
+
+    public void AddOneMinute()
+    {
+        AddSeconds(60);
+    }
+
+    public void AddOneHour()
+    {
+        AddSeconds(3600);
+    }
+
+    public void AddSeconds(int s)
+    {
+        int totalSeconds = TimeSinceMidnight() + s;
+        MyTime newTime = TimeSinceMidnight(totalSeconds);
+        this.hour = newTime.hour;
+        this.minute = newTime.minute;
+        this.second = newTime.second;
+    }
+
+    public static int Difference(MyTime mt1, MyTime mt2)
+    {
+        return mt1.TimeSinceMidnight() - mt2.TimeSinceMidnight();
+    }
+
+    public string WhatLesson()
+    {
+        int seconds = TimeSinceMidnight();
+
+        if (seconds < 8 * 3600) return "Пари ще не почалися";
+        if (seconds < 9 * 3600 + 20 * 60) return "1-а пара";
+        if (seconds < 9 * 3600 + 30 * 60) return "Перерва між 1-ю та 2-ю парами";
+        if (seconds < 10 * 3600 + 50 * 60) return "2-а пара";
+        if (seconds < 11 * 3600 + 00 * 60) return "Перерва між 2-ю та 3-ю парами";
+        if (seconds < 12 * 3600 + 20 * 60) return "3-я пара";
+        if (seconds < 12 * 3600 + 30 * 60) return "Перерва між 3-ю та 4-ю парами";
+        if (seconds < 13 * 3600 + 50 * 60) return "4-а пара";
+        if (seconds < 14 * 3600 + 00 * 60) return "Перерва між 4-ю та 5-ю парами";
+        if (seconds < 15 * 3600 + 20 * 60) return "5-а пара";
+        if (seconds < 15 * 3600 + 30 * 60) return "Перерва між 5-ю та 6-ю парами";
+        if (seconds < 16 * 3600 + 50 * 60) return "6-а пара";
+
+        return "Пари вже скінчилися";
+    }
+}
+
---
+++ OOP_lab2_2/OOP_lab2_2/Program.cs
@@ -0,0 +1,50 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding=System.Text.Encoding.UTF8;
+        Console.Write("Введіть годину: ");
+        int hour = int.Parse(Console.ReadLine());
+        Console.Write("Введіть хвилини: ");
+        int minute = int.Parse(Console.ReadLine());
+        Console.Write("Введіть секунди: ");
+        int second = int.Parse(Console.ReadLine());
+
+        MyTime userTime = new MyTime(hour, minute, second);
+        Console.WriteLine("Введений час: " + userTime.ToString());
+
+        int secondsSinceMidnight = userTime.TimeSinceMidnight();
+        Console.WriteLine("Кількість секунд від початку доби: " + secondsSinceMidnight);
+               
+        MyTime timeFromSeconds = MyTime.TimeSinceMidnight(secondsSinceMidnight);
+        Console.WriteLine("Час з кількості секунд: " + timeFromSeconds.ToString());
+                
+        userTime.AddOneSecond();
+        Console.WriteLine("Час після додавання однієї секунди: " + userTime.ToString());
+        
+        userTime.AddOneMinute();
+        Console.WriteLine("Час після додавання однієї хвилини: " + userTime.ToString());
+                
+        userTime.AddOneHour();
+        Console.WriteLine("Час після додавання однієї години: " + userTime.ToString());
+                
+        Console.Write("Введіть кількість секунд для додавання: ");
+        int additionalSeconds = int.Parse(Console.ReadLine());
+        userTime.AddSeconds(additionalSeconds);
+        Console.WriteLine("Час після додавання секунд: " + userTime.ToString());
+        
+        Console.Write("Введіть годину для другого часу: ");
+        int hour2 = int.Parse(Console.ReadLine());
+        Console.Write("Введіть хвилини для другого часу: ");
+        int minute2 = int.Parse(Console.ReadLine());
+        Console.Write("Введіть секунди для другого часу: ");
+        int second2 = int.Parse(Console.ReadLine());
+
+        MyTime userTime2 = new MyTime(hour2, minute2, second2);
+        int difference = MyTime.Difference(userTime, userTime2);
+        Console.WriteLine("Різниця між введеними часами в секундах: " + difference);
+      
+        string lesson = userTime.WhatLesson();
+        Console.WriteLine("Поточний урок: " + lesson);
+    }
+}
